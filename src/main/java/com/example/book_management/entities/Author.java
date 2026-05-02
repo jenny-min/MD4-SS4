cb9d0e3e3922baaa3aac7393e2cb144f7d6d1f14
jenny-min <jennynguyen.92.92@gmail.com>
2026-05-02 10:09:09 +0900

    ss4 exercise01

---
+++ src/main/java/com/example/book_management/entities/Author.java
@@ -0,0 +1,39 @@
+package com.example.book_management.entity;
+
+public class Author {
+    long id;
+    String name;
+    String email;
+
+    public Author() {}
+
+    public Author(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/book_management/repositories/AuthorRepository.java
@@ -0,0 +1,4 @@
+package com.example.book_management.repositories;
+
+public class AuthorRepository {
+}
